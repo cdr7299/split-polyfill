a30ca49be6e76778af195a1c5ac668a0b394b0d6
Vineet Sawhney <gadgetgeek95@gmail.com>
2022-07-06 19:53:48 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+function customSplit(str, delim = "") {
+  let delimLen = delim.length;
+  let resultIndices = [];
+  let resultArray = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let chunk = str.substr(i, delimLen);
+    if (chunk === delim) resultIndices.push(i);
+  }
+
+  resultArray.push(str.substr(0, resultIndices[0]));
+  for (let i = 0; i < resultIndices.length - 1; i++) {
+    resultArray.push(
+      str.substr(
+        resultIndices[i] + delimLen,
+        resultIndices[i + 1] - resultIndices[i] - delimLen
+      )
+    );
+  }
+  resultArray.push(
+    str.substr(resultIndices[resultIndices.length - 1] + delimLen)
+  );
+}
+
+let str = "abc abc abc";
+customSplit(str, " ");
